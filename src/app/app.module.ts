6a10082c03d7699e7cc7faf3ff6fceea0826f676
Saketh Susarla <venkata.susarla@siemens.com>
2024-10-08 09:16:53 +0530

    minimal app (16)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IxModule } from '@siemens/ix-angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import '@siemens/ix-echarts';
 import * as echarts from 'echarts/core';
 import { NgxEchartsModule } from 'ngx-echarts';
 import { AppComponent } from './app.component';
